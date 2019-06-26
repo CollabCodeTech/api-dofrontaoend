22972e587c2f5e9736f70bb5f56354cfd2762bc8
Marco Bruno <marco.bruno.br@gmail.com>
2019-06-26 01:17:29 +0100

    Teste do pre-inscription rodando
    Vamo que vamo

--- src/pre-inscription/pre-inscription.controller.spec.ts
+++ src/pre-inscription/pre-inscription.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PreInscriptionController } from './pre-inscription.controller';
+import { PreInscriptionService } from './pre-inscription.service';
 
 describe('PreInscription Controller', () => {
   let controller: PreInscriptionController;
@@ -7,12 +8,14 @@ describe('PreInscription Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PreInscriptionController],
+      providers: [PreInscriptionService]
     }).compile();
 
     controller = module.get<PreInscriptionController>(PreInscriptionController);
   });
 
   it('should be defined', () => {
+    console.log(controller);
     expect(controller).toBeDefined();
   });
 });
